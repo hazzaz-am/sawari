134276a9936aacf1721b3a707d58cbfa3c60469b
hazzaz-am <hazzazabdulmannan@gmail.com>
2026-01-26 18:47:12 +0600

    feat: complete nativewind setup for tailwind support

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,34 @@
 import { Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
 	return (
-		<SafeAreaView>
-			<View className="flex-1 items-center justify-center bg-red-300
-      ">
-				<Text className="text-xl font-bold text-blue-500 text-center">Welcome to Nativewind!</Text>
-			</View>
-		</SafeAreaView>
+		<View
+			className="flex-1 items-center justify-center bg-red-500
+      "
+		>
+			<Text className="text-xl font-bold text-white text-center">Welcome to Nativewind!</Text>
+			<Text className="text-xl font-bold text-white text-center">Welcome to Nativewind!</Text>
+			<Text className="text-xl font-bold text-yellow-400 text-center">Welcome to Nativewind!</Text>
+			<Text className="text-xl font-bold text-white text-center">Welcome to Nativewind!</Text>
+			<Text className="text-xl font-bold text-blue-500 text-center">Welcome to Nativewind!</Text>
+			<Text className="text-xl font-bold text-white text-center">Welcome to Nativewind!</Text>
+			<Text className="text-xl font-bold text-white text-center">Welcome to Nativewind!</Text>
+			<Text className="text-xl font-bold text-white text-center">Welcome to Nativewind!</Text>
+			<Text className="text-xl font-bold text-white text-center">Welcome to Nativewind!</Text>
+			<Text className="text-xl font-bold text-white text-center">Welcome to Nativewind!</Text>
+			<Text className="text-xl font-bold text-white text-center">Welcome to Nativewind!</Text>
+			<Text className="text-xl font-bold text-white text-center">Welcome to Nativewind!</Text>
+			<Text className="text-xl font-bold text-white text-center">Welcome to Nativewind!</Text>
+			<Text className="text-xl font-bold text-white text-center">Welcome to Nativewind!</Text>
+			<Text className="text-xl font-bold text-white text-center">Welcome to Nativewind!</Text>
+			<Text className="text-xl font-bold text-white text-center">Welcome to Nativewind!</Text>
+			<Text className="text-xl font-bold text-white text-center">Welcome to Nativewind!</Text>
+			<Text className="text-xl font-bold text-white text-center">Welcome to Nativewind!</Text>
+			<Text className="text-xl font-bold text-white text-center">Welcome to Nativewind!</Text>
+			<Text className="text-xl font-bold text-white text-center">Welcome to Nativewind!</Text>
+			<Text className="text-xl font-bold text-white text-center">Welcome to Nativewind!</Text>
+			<Text className="text-xl font-bold text-white text-center">Welcome to Nativewind!</Text>
+			<Text className="text-xl font-bold text-white text-center">Welcome to Nativewind!</Text>
+		</View>
 	);
 }
