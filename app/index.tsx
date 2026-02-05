7ddfea0c0a493be7d0487f37eea5ed6d3afbae61
hazzaz-am <hazzazabdulmannan@gmail.com>
2026-02-05 19:17:38 +0600

    feat: add root and auth pages

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,5 @@
-import { StatusBar, Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Redirect } from "expo-router";
 
-export default function Index() {
-	return (
-		<SafeAreaView>
-			<Text className="text-xl font-bold text-center">Welcome to Nativewind!</Text>
-			<StatusBar />
-		</SafeAreaView>
-	);
+export default function Home() {
+	return <Redirect href="/(auth)/welcome"/>
 }
---
+++ app/root/(tabs)/_layout.tsx
@@ -0,0 +1,5 @@
+export default function TabsLayout() {
+  return (
+    <div>TabsLayout</div>
+  )
+}
